5553f833676aee14c10ccfebdc51d4e5d395d3c4
lamba <eclipjke@mail.ru>
2021-06-14 13:11:31 +0300

    Websocket and broadcasting

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -2,4 +2,5 @@ Echo
   .channel('hello')
   .listen('ArticleModified', (e) => {
     alert('тест вэб сокет соединения')
+    console.log('sdfsdf')
   });
--- routes/channels.php
+++ routes/channels.php
@@ -17,6 +17,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('hello', function ($user, $id) {
+Broadcast::channel('hello', function ($article, $id) {
     return true;
 });
